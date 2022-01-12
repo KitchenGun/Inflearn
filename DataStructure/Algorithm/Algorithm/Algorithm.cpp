3d463daef36437fcb358bec22ed3e50f250c711d
KitchenGun <kitchengun124@gmail.com>
2022-01-12 19:26:55 +0900

    List와 template으로 list를 구현하여 공부
    섹션:2 연결리스트 구현 연습

--- DataStructure/Algorithm/Algorithm/Algorithm.cpp
+++ DataStructure/Algorithm/Algorithm/Algorithm.cpp
@@ -1,7 +1,9 @@
 ﻿#include <iostream>
 #include <vector>
+#include <list>
 using namespace std;
 
+#ifdef vector
 template<typename T>
 class Vector
 {
@@ -41,7 +43,7 @@ public:
 		_capacity = capacity;
 		T* newData = new T[_capacity];
 
-		for (int i =0;i< _size;i++)
+		for (int i = 0; i < _size; i++)
 		{
 			newData[i] = _data[i];
 		}
@@ -66,16 +68,171 @@ public:
 		_size = 0;
 	}
 private:
-	
+
 	T* _data = nullptr;
 	int _size = 0;
 	int _capacity = 0;
+
+};
+#endif // vector
+
+template <typename T>
+class Node
+{
+public:
+	Node() :_prev(nullptr), _next(nullptr), _data(T())
+	{
+
+	}
+	Node(const T& value) :_prev(nullptr), _next(nullptr), _data(value)
+	{
+	}
+public:
+	Node* _prev;
+	Node* _next;
+	T _data;
+};
+
+template <typename T>
+class Iterator
+{
+public:
+	Iterator() :_node(nullptr)
+	{
+
+	}
+
+	Iterator(Node<T>* node) :_node(node)
+	{
+
+	}
+	//++it
+	Iterator& operator++()
+	{
+		_node = _node->_next;
+		return *this;
+	}
+	//--it
+	Iterator operator++(int)
+	{
+		Iterator<T> temp = *this;
+		_node = _node->_next;
+		return temp;
+	}
+	Iterator& operator--()
+	{
+		_node = _node->_prev;
+		return *this;
+	}
+	Iterator operator--(int)
+	{
+		Iterator<T> temp = *this;
+		_node = _node->_prev;
+		return temp;
+	}
+	//*it
+	T& operator*()
+	{
+		return _node->_data;
+	}
+
+	bool operator==(const Iterator& other)
+	{
+		return this->_node == other._node;
+	}
+	bool operator!=(const Iterator& other)
+	{
+		return this->_node != other._node;
+	}
+public:
+	Node<T>* _node;
+
+};
+
+template <typename T>
+class List
+{
+public:
+	List() :_size(0)
+	{
+		_head = new Node<T>();
+		_tail = new Node<T>();
+		_head->_next = _tail;
+		_tail->_prev = _head;
+	}
+	~List()
+	{
+		while (_size > 0)
+			pop_back();
+
+		delete _head;
+		delete _tail;
+	}
+
+	void push_back(const T& value)
+	{
+		AddNode(_tail, value);
+	}
+	void pop_back()
+	{
+		RemoveNode(_tail->_prev);
+	}
+private:
+	Node<T>* AddNode(Node<T>* before, const T& value)
+	{
+		Node<T>* newNode = new Node<T>(value);
+		Node<T>* prevNode = before->_prev;
+		prevNode->_next = newNode;
+		newNode->_prev = prevNode;
+
+		newNode->_next = before;
+		before->_prev = newNode;
+		
+		_size++;
+
+		return newNode;
+	}
+
+	Node<T>* RemoveNode(Node<T>* node)
+	{
+		Node<T>* prevNode = node->_prev;
+		Node<T>* nextNode = node->_next;
+
+		prevNode->_next = nextNode;
+		nextNode->_prev = prevNode;
+		
+		delete node;
+		_size--;
+		return nextNode;
+ 	}
+	int size() { return _size; }
+public:
+	Node<T>* _head;
+	Node<T>* _tail;
+	int _size;
+
+public:
+	using iterator = Iterator<T>;
+	iterator begin() { return iterator(_head->_next); }
+	iterator end() { return iterator(_tail); }
+
+	iterator insert(iterator it, const T& value)
+	{
+		Node<T>* node = AddNode(it._node, value);
+		return iterator(node);
+	}
+	iterator erase(iterator it)
+	{
+		Node<T>* node = RemoveNode(it._node);
+		return iterator(node);
+	}
 
 };
 
 
 int main()
 {
+#ifdef vector
 	vector<int> v;
 	
 	//reserve는 capacity를 조절하는 것이고
@@ -90,4 +247,34 @@ int main()
 	v.resize(10, 0);
 	v.clear();//클리어해도 크기가 줄지 않는다
 	cout <<v.size() << " " << v.capacity() << endl;//size는 가지고 있는 데이터 //capacity는 벡터의 크기 용량
+
+#endif // vector
+	
+	List<int> li;
+	List<int>::iterator eraselt;
+
+	for (int i = 0; i < 10; i++)
+	{
+		if (i == 5)
+		{
+			eraselt = li.insert(li.end(), i);
+		}
+		li.push_back(i);
+	}
+	for (List<int>::iterator it = li.begin(); it != li.end(); it++)
+	{
+		cout << ((it)._node->_data) << endl;
+	}
+	cout << endl;
+	li.pop_back();
+	li.erase(eraselt);
+
+
+	for (List<int>::iterator it = li.begin(); it != li.end(); it++)
+	{
+		cout << ((it)._node->_data) << endl;
+	}
+
+
+
 }
