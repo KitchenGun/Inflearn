afccc68753dc0b0a5ec516669c70d23c17e4bfc4
Keon Kang <kitchengun124@gmail.com>
2024-01-30 22:28:47 +0900

    2차원 배열 문제
    vector 2차원 재정의 삽입 순환

--- Review/Algorithm/Study/Empty.cpp
+++ Review/Algorithm/Study/Empty.cpp
@@ -1,9 +1,49 @@
 #include <iostream>
-
+#include <vector>
+#include <string>
 using namespace std;
 
 int main()
 {
-	cout << "Hello World!" << endl;
+	int x=0,y=0;
+	cin >> x >> y;
+	vector<vector<int>> arr;
+	arr.resize(x);
+	cin.ignore();
+	for (int i = 0; i < x; i++)
+	{
+		arr[i].resize(y);
+	}
+
+	for (int i = 0; i < x; i++)
+	{
+		for (int j = 0; j < y; j++)
+		{
+			int temp;
+			cin >> temp;
+			arr[i][j] = temp;
+		}
+	}
+
+	for (int i = 0; i < x; i++)
+	{
+		for (int j = 0; j < y; j++)
+		{
+			int temp;
+			cin>>temp;
+			arr[i][j] += temp;
+		}
+	}
+
+
+	for (int i = 0; i < x; i++)
+	{
+		for (int j = 0; j < y; j++)
+		{
+			cout<<arr[i][j]<<" ";
+		}
+		cout << endl;
+	}
+
 	return 0;
 }
