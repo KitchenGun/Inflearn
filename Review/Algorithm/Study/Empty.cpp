f23fde908134251ef4897f675cf2886c9fab1efe
Keon Kang <kitchengun124@gmail.com>
2024-03-03 13:36:00 +0900

    long 자료형 사용 문제
    https://www.acmicpc.net/problem/15894

--- Review/Algorithm/Study/Empty.cpp
+++ Review/Algorithm/Study/Empty.cpp
@@ -7,27 +7,7 @@ using namespace std;
 
 int main()
 {
-	int n;
-	vector<vector<bool>> v(100, vector<bool>(100));
-	cin >> n;
-	int result = 0;
-	for (int i = 0; i < n; i++)
-	{
-		int a=0, b=0;
-		
-		cin>>a >> b;
-		int amax=a+10,bmax=b+10;
-		for (int x = a; x < amax; x++)
-		{
-			for (int y = b; y < bmax; y++)
-			{
-				if (v[x][y] == 0)
-				{
-					v[x][y] = 1;
-					result++;
-				}
-			}
-		}
-	}
-	cout<<result;
+	long a;
+	cin >> a;
+	cout << a*4;
 }
