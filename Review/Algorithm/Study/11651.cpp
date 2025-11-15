0d46f80f3c708aa6f9db660fae4cfa6b3b79c317
Keon Kang <kitchengun124@gmail.com>
2025-11-15 19:12:21 +0900

    백준 좌표 정렬하기 2
    11651 문제 풀기

---
+++ Review/Algorithm/Study/11651.cpp
@@ -0,0 +1,25 @@
+#include "iostream"
+#include "vector"
+#include "algorithm"
+
+using namespace std;
+
+
+int main()
+{
+	int n;
+	cin >> n;
+	vector<pair<int, int>> v;
+	for (int i = 0; i < n; i++)
+	{
+		int x, y;
+		cin >> x >> y;
+		v.push_back({ y, x });
+	}
+	sort(v.begin(), v.end());
+	for (auto var : v)
+	{
+		cout << var.second << " " << var.first << "\n";
+	}
+	return 0;
+}
